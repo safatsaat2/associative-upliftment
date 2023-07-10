2adc9eb7c7cf555a46eddeb12b8b7d283191c622
Sakib Rahman Safat <sakibsafat47@gmail.com>
2023-07-10 04:31:40 -0700

    Installed: react, react-router, tailwind,

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,12 @@
+import {
+    createBrowserRouter,
+  } from "react-router-dom";
+
+
+export const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <div className="text-center text-4xl">My name is Khan</div>,
+    },
+  ]);
+
