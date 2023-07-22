775a1e302a837f57c06bc26b6dffebfe20aef630
Sakib Rahman Safat <sakibsafat47@gmail.com>
2023-07-22 23:45:59 +0600

    payment method added

--- src/Page/Dashboard/Dashboard.jsx
+++ src/Page/Dashboard/Dashboard.jsx
@@ -1,13 +1,26 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../Provider/AuthProvider";
+import { loadStripe } from "@stripe/stripe-js";
+import axios from "axios";
 
 const Dashboard = () => {
-  const { user } = useContext(AuthContext);
-  console.log(user);
+  const { user } = useContext(AuthContext); 
+  const [stripePromise, setStripePromise] = useState(null);
+  const [clientSecret, setClientSecret] = useState("");
+  
+   useEffect(() =>{
+    fetch("http://localhost:7000/config").then(async(r) =>{
+      const {publishableKey} = await r.json()
+      setStripePromise(loadStripe(publishableKey))
+    })
+   }, [])
+  const handleCheckout = async ( )=>{
+    
+  }
   return (
         <div className="flex flex-col items-center justify-end lg:h-[250px]">
           <h1 className="text-6xl font-medium">Welcome {user?.displayName},</h1>
-          <button className="bg-black mt-4 text-white rounded-lg lg:w-[217px] lg:h-[48px]">
+          <button onClick={handleCheckout} className="bg-black mt-4 text-white rounded-lg lg:w-[217px] lg:h-[48px]">
             Active your Account
           </button>
         </div>
