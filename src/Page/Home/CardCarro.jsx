2df3137463a17e88b7ae9f4fbc1992de9a4c752f
Sakib Rahman Safat <sakibsafat47@gmail.com>
2023-07-31 00:38:03 +0600

    carrosole added new

--- src/Page/Home/CardCarro.jsx
+++ src/Page/Home/CardCarro.jsx
@@ -7,23 +7,27 @@ const CardCarro = () => {
       img: "https://i.ibb.co/1KXd596/image.png",
       title: "Book Cover Design",
       user: "Strategy Stars Team",
-      userPic: "https://i.ibb.co/Hr8wSJK/Whats-App-Image-2023-07-31-at-00-26-32.jpg",
+      userPic:
+        "https://i.ibb.co/Hr8wSJK/Whats-App-Image-2023-07-31-at-00-26-32.jpg",
       star: 3,
     },
     {
       id: "2",
-      img: "https://i.ibb.co/nBG5sHm/image.png",
-      title: "Full Stack Development",
-      user: "Dragon Squad Team",
-      userPic: "",
+      img: "https://i.ibb.co/ZcMSZww/image.png",
+      title: "Magazines Design",
+      user: "Dynamic Squad Team",
+      userPic:
+        "https://i.ibb.co/z4SKbDZ/Whats-App-Image-2023-07-31-at-00-21-00.jpg",
       star: 4,
     },
+
     {
       id: "1",
       img: "https://i.ibb.co/gZ3yvXX/Frame-4141.png",
       title: "UI/UX Design",
       user: "Galaxy Gliders Team",
-      userPic: "https://i.ibb.co/kcH9ntK/Whats-App-Image-2023-07-31-at-00-28-59.jpg",
+      userPic:
+        "https://i.ibb.co/kcH9ntK/Whats-App-Image-2023-07-31-at-00-28-59.jpg",
       star: 5,
     },
   ];
@@ -33,15 +37,16 @@ const CardCarro = () => {
       img: "https://i.ibb.co/nRPh3Pm/image.png",
       title: "Illustration Design",
       user: "Titans Alliance Team",
-      userPic: "https://i.ibb.co/0cJwXHp/Whats-App-Image-2023-07-31-at-00-23-41.jpg",
+      userPic:
+        "https://i.ibb.co/0cJwXHp/Whats-App-Image-2023-07-31-at-00-23-41.jpg",
       star: 6,
     },
     {
       id: "2",
-      img: "https://i.ibb.co/ZcMSZww/image.png",
-      title: "Magazines Design",
-      user: "Dynamic Squad Team",
-      userPic: "https://i.ibb.co/z4SKbDZ/Whats-App-Image-2023-07-31-at-00-21-00.jpg",
+      img: "https://i.ibb.co/nBG5sHm/image.png",
+      title: "Full Stack Development",
+      user: "Dragon Squad Team",
+      userPic: "https://i.ibb.co/svJgchW/Whats-App-Image-2023-07-31-at-00-33-33.jpg",
       star: 7,
     },
     {
@@ -49,7 +54,7 @@ const CardCarro = () => {
       img: "https://i.ibb.co/J3NdKz9/image.png",
       title: "Photography Product",
       user: "Galactic Guardians Team",
-      userPic: "",
+      userPic: "https://i.ibb.co/sKMJXX4/Whats-App-Image-2023-07-31-at-00-35-34.jpg",
       star: 8,
     },
   ];
@@ -60,7 +65,7 @@ const CardCarro = () => {
           {slide1.map((card) => (
             <div
               key={card.id}
-              className="mx-5 shadow-lg rounded-xl bg-slate-50"
+              className="mx-5 shadow-lg rounded-xl bg-slate-50 hover:translate-y-2 duration-300"
             >
               <img
                 className="w-[400px] h-[350px] rounded-t-xl"
@@ -69,13 +74,13 @@ const CardCarro = () => {
               />
               <div className="flex items-center my-2">
                 <img
-                  className="w-16 h-16 ml-5 my-5 rounded-full"
+                  className="w-16 h-16 ml-5  rounded-full"
                   src={card.userPic}
                   alt=""
                 />
                 <div className="ml-5">
                   <p className="font-semibold text-lg">{card.title}</p>
-                  <p>by {card.user}</p>
+                  <p>By {card.user}</p>
                   <ReactStars
                     value={card.star}
                     count={8}
@@ -116,7 +121,7 @@ const CardCarro = () => {
                 />
                 <div className="ml-5">
                   <p className="font-semibold text-lg">{card.title}</p>
-                  <p>by {card.user}</p>
+                  <p>By {card.user}</p>
                   <ReactStars
                     value={card.star}
                     count={8}
