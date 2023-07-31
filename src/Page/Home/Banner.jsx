15c18ec87e6c04bd0dbd63cdeeb51ec84a318246
Sakib Rahman Safat <sakibsafat47@gmail.com>
2023-07-31 21:42:51 +0600

    Banner done

--- src/Page/Home/Banner.jsx
+++ src/Page/Home/Banner.jsx
@@ -4,14 +4,14 @@ import { Link } from "react-router-dom";
 const Banner = () => {
     return (
         <div className="bg-black w-full rounded-3xl mt-8">
-            <div className="lg:flex lg:justify-between items-center w-full p-6">
+            <div className="flex flex-col-reverse lg:flex lg:flex-row lg:justify-between items-center w-full p-6">
                 <div className="text-white ">
-                    <h1 className="lg:text-[100px] font-sans font-semibold leading-[115px] pb-4 w-full lg:w-[710px]">Grow Your <span className="text-[#1A9755]">Business</span> By Joining us</h1>
+                    <h1 className="text-4xl lg:text-[100px] font-sans font-semibold lg:leading-[115px] pb-4 w-full lg:w-[710px]">Grow Your <span className="text-[#1A9755]">Business</span> By Joining us</h1>
                     <p className="text-white text-[20px] w-full lg:w-[685px] pb-4">You just give your product information. We will arrange it and present it to the audience.</p>
-                    <Link to='/signup'><button className='bg-white text-black rounded-lg lg:w-[217px] h-[48px] font-semibold'>Get Started</button></Link>
+                    <Link to='/signup'><button className='bg-white px-2 lg:px-0 text-black rounded-lg lg:w-[217px] h-[48px] font-semibold'>Get Started</button></Link>
                 </div>
                 <div data-aos="zoom-in-up" data-aos-duration="1000"> 
-                    <img  className="w-full" src="https://i.ibb.co/Zzr7whS/image-3.png" alt="" />
+                    <img  className="w-full mb-6 lg:mb-0" src="https://i.ibb.co/Zzr7whS/image-3.png" alt="" />
                 </div>
             </div>
         </div>
