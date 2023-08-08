c7a9c50cc9fc9b0cab261df9b98220801da8a2ce
Sakib Rahman Safat <sakibsafat47@gmail.com>
2023-08-09 00:02:36 +0600

    popular service in home link added

--- src/Page/Carro/Carro.jsx
+++ src/Page/Carro/Carro.jsx
@@ -7,19 +7,19 @@ const Carro = () => {
         <h1 className="text-5xl text-center font-bold pb-8">Popular Services</h1>
         <div className="carousel w-full mb-20">
           <div id="slide1" className="carousel-item w-full">
-            <Link className="w-full ml-4">
+            <Link to="/popularservice/1" className="w-full ml-4">
               <img src="https://i.ibb.co/LtCNz8j/Group-1000003431-1.png" className="rounded-md" />
             </Link>
-            <Link to="" className="w-full ml-4">
+            <Link to="/popularservice/2" className="w-full ml-4">
               <img src="https://i.ibb.co/HN0n25v/Group-1000003432-2.png" className="rounded-md" />
             </Link>
-            <Link to="" className="w-full ml-4">
+            <Link to="/popularservice/3" className="w-full ml-4">
               <img src="https://i.ibb.co/6W6JfmM/Group-1000003433-1.png" className="rounded-md" />
             </Link>
-            <Link to="" className="w-full ml-4">
+            <Link to="/popularservice/4" className="w-full ml-4">
               <img src="https://i.ibb.co/m9cCsG9/Group-1000003434-1.png" className="rounded-md" />
             </Link>
-            <Link to="" className="w-full ml-4">
+            <Link to="/popularservice/5" className="w-full ml-4">
               <img src="https://i.ibb.co/BLsCxjM/Group-1000003435-1.png" className="rounded-md" />
             </Link>
 
@@ -35,19 +35,19 @@ const Carro = () => {
             </div>
           </div>
           <div id="slide2" className="carousel-item relative w-full">
-            <Link to="" className="w-full ml-4">
+            <Link to="/popularservice/6" className="w-full ml-4">
               <img src="https://i.ibb.co/Y3RLvs3/Group-1000003431-2.png" className="rounded-md" />
             </Link>
-            <Link to="0" className="w-full ml-4">
+            <Link to="/popularservice/7" className="w-full ml-4">
               <img src="https://i.ibb.co/2nBPsF0/Group-1000003432-3.png" className="rounded-md" />
             </Link>
-            <Link to="" className="w-full ml-4">
+            <Link to="/popularservice/8" className="w-full ml-4">
               <img src="https://i.ibb.co/XFLcMgr/Group-1000003433-2.png" className="rounded-md" />
             </Link>
-            <Link to="" className="w-full ml-4">
+            <Link to="/popularservice/9" className="w-full ml-4">
               <img src="https://i.ibb.co/ZhMRhmk/Group-1000003434-2.png" className="rounded-md" />
             </Link>
-            <Link to="" className="w-full ml-4">
+            <Link to="/popularservice/10" className="w-full ml-4">
               <img src="https://i.ibb.co/nLDRKxJ/Group-1000003435-2.png" className="rounded-md" />
             </Link>
 
@@ -64,12 +64,13 @@ const Carro = () => {
           </div>
         </div>
       </div>
+      {/* Mobile Responsive */}
       <div className="block mx-auto w-full lg:hidden">
-        <h1 className="text-3xl text-center font-bold pb-8">Our Shop</h1>
+        <h1 className="text-3xl text-center font-bold pb-8">Popular Services</h1>
         <div className="carousel w-full mb-20">
-          <h1 className="text-3xl text-center font-bold pb-8">Our Shop</h1>
+          <h1 className="text-3xl text-center font-bold pb-8">Popular Services</h1>
           <div id="slide5" className="carousel-item relative w-full">
-            <Link className="w-full mx-20">
+            <Link to='/popularservice/1' className="w-full mx-20">
               <img src="https://i.ibb.co/LtCNz8j/Group-1000003431-1.png" className="rounded-md" />
             </Link>
             <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
@@ -82,7 +83,7 @@ const Carro = () => {
             </div>
           </div>
           <div id="slide6" className="carousel-item relative w-full">
-            <Link to="" className="w-full mx-20">
+            <Link to="/popularservice/2" className="w-full mx-20">
               <img src="https://i.ibb.co/HN0n25v/Group-1000003432-2.png" className="rounded-md" />
             </Link>
             <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
@@ -95,7 +96,7 @@ const Carro = () => {
             </div>
           </div>
           <div id="slide7" className="carousel-item relative w-full">
-            <Link to="" className="w-full mx-20">
+            <Link to="/popularservice/3" className="w-full mx-20">
               <img src="https://i.ibb.co/6W6JfmM/Group-1000003433-1.png" className="rounded-md" />
             </Link>
             <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
@@ -108,7 +109,7 @@ const Carro = () => {
             </div>
           </div>
           <div id="slide8" className="carousel-item relative w-full">
-            <Link to="" className="w-full mx-20">
+            <Link to="/popularservice/4" className="w-full mx-20">
               <img src="https://i.ibb.co/m9cCsG9/Group-1000003434-1.png" className="rounded-md" />
             </Link>
             <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
@@ -121,7 +122,7 @@ const Carro = () => {
             </div>
           </div>
           <div id="slide9" className="carousel-item relative w-full">
-            <Link to="" className="w-full mx-20">
+            <Link to="/popularservice/5" className="w-full mx-20">
               <img src="https://i.ibb.co/BLsCxjM/Group-1000003435-1.png" className="rounded-md" />
             </Link>
             <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
@@ -134,7 +135,7 @@ const Carro = () => {
             </div>
           </div>
           <div id="slide10" className="carousel-item relative w-full">
-            <Link to="" className="w-full mx-20">
+            <Link to="/popularservice/6" className="w-full mx-20">
               <img src="https://i.ibb.co/Y3RLvs3/Group-1000003431-2.png" className="rounded-md" />
             </Link>
             <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
@@ -147,7 +148,7 @@ const Carro = () => {
             </div>
           </div>
           <div id="slide11" className="carousel-item relative w-full">
-            <Link to="0" className="w-full mx-20">
+            <Link to="/popularservice/7" className="w-full mx-20">
               <img src="https://i.ibb.co/2nBPsF0/Group-1000003432-3.png" className="rounded-md" />
             </Link>
             <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
@@ -160,7 +161,7 @@ const Carro = () => {
             </div>
           </div>
           <div id="slide12" className="carousel-item relative w-full">
-            <Link to="" className="w-full mx-20">
+            <Link to="/popularservice/8" className="w-full mx-20">
               <img src="https://i.ibb.co/XFLcMgr/Group-1000003433-2.png" className="rounded-md" />
             </Link>
             <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
@@ -173,7 +174,7 @@ const Carro = () => {
             </div>
           </div>
           <div id="slide13" className="carousel-item relative w-full">
-            <Link to="" className="w-full mx-20">
+            <Link to="/popularservice/9" className="w-full mx-20">
               <img src="https://i.ibb.co/ZhMRhmk/Group-1000003434-2.png" className="rounded-md" />
             </Link>
             <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
@@ -186,7 +187,7 @@ const Carro = () => {
             </div>
           </div>
           <div id="slide14" className="carousel-item relative w-full">
-            <Link to="" className="w-full mx-20">
+            <Link to="/popularservice/10" className="w-full mx-20">
               <img src="https://i.ibb.co/nLDRKxJ/Group-1000003435-2.png" className="rounded-md" />
             </Link>
             <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
--- src/Page/Service/PopularService.jsx
+++ src/Page/Service/PopularService.jsx
@@ -1,6 +1,9 @@
 import { Link } from "react-router-dom";
 
 const PopularService = () => {
+    const toTop = () => {
+        window.scrollTo(0, 0);
+      };
     return (
         <div className="mt-20">
             <h1
@@ -18,18 +21,36 @@ const PopularService = () => {
                 Delivering Exceptional Solutions
             </p>
             <div className="grid grid-cols-1 lg:grid-cols-3 gap-x-[92px] gap-y-[75px] place-items-center max-w-7xl mx-auto">
-                <Link to="/popularservice/1">
-                    <img src="https://i.ibb.co/LtCNz8j/Group-1000003431-1.png" className="rounded-md" />
+                <Link  to="/popularservice/1">
+                    <img onClick={toTop} src="https://i.ibb.co/LtCNz8j/Group-1000003431-1.png" className="rounded-md" />
                 </Link>
-                <img src="https://i.ibb.co/HN0n25v/Group-1000003432-2.png" className="rounded-md" />
-                <img src="https://i.ibb.co/6W6JfmM/Group-1000003433-1.png" className="rounded-md" />
-                <img src="https://i.ibb.co/m9cCsG9/Group-1000003434-1.png" className="rounded-md" />
-                <img src="https://i.ibb.co/BLsCxjM/Group-1000003435-1.png" className="rounded-md" />
-                <img src="https://i.ibb.co/Y3RLvs3/Group-1000003431-2.png" className="rounded-md" />
-                <img src="https://i.ibb.co/2nBPsF0/Group-1000003432-3.png" className="rounded-md" />
-                <img src="https://i.ibb.co/XFLcMgr/Group-1000003433-2.png" className="rounded-md" />
-                <img src="https://i.ibb.co/ZhMRhmk/Group-1000003434-2.png" className="rounded-md" />
-                <img src="https://i.ibb.co/nLDRKxJ/Group-1000003435-2.png" className="rounded-md" />
+                <Link to="/popularservice/2">
+                    <img src="https://i.ibb.co/HN0n25v/Group-1000003432-2.png" className="rounded-md" />
+                </Link>
+                <Link to="/popularservice/3">
+                    <img src="https://i.ibb.co/6W6JfmM/Group-1000003433-1.png" className="rounded-md" />
+                </Link>
+                <Link to="/popularservice/4">
+                    <img src="https://i.ibb.co/m9cCsG9/Group-1000003434-1.png" className="rounded-md" />
+                </Link>
+                <Link to="/popularservice/5">
+                    <img src="https://i.ibb.co/BLsCxjM/Group-1000003435-1.png" className="rounded-md" />
+                </Link>
+                <Link to="/popularservice/6">
+                    <img src="https://i.ibb.co/Y3RLvs3/Group-1000003431-2.png" className="rounded-md" />
+                </Link>
+                <Link to="/popularservice/7">
+                    <img src="https://i.ibb.co/2nBPsF0/Group-1000003432-3.png" className="rounded-md" />
+                </Link>
+                <Link to="/popularservice/8">
+                    <img src="https://i.ibb.co/XFLcMgr/Group-1000003433-2.png" className="rounded-md" />
+                </Link>
+                <Link to="/popularservice/9">
+                    <img src="https://i.ibb.co/ZhMRhmk/Group-1000003434-2.png" className="rounded-md" />
+                </Link>
+                <Link to="/popularservice/10">
+                    <img src="https://i.ibb.co/nLDRKxJ/Group-1000003435-2.png" className="rounded-md" />
+                </Link>
             </div>
         </div>
     );
