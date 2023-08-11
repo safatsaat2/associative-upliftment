58d67db4635084c666ae90d406bbf0d442a45920
Sakib Rahman Safat <sakibsafat47@gmail.com>
2023-08-11 22:39:43 +0600

    dashboard home created

--- src/Page/Dashboard/Dashboard.jsx
+++ src/Page/Dashboard/Dashboard.jsx
@@ -1,17 +1,29 @@
-import { useContext,  useState } from "react";
-import { AuthContext } from "../../Provider/AuthProvider";
-// import CheckoutFom from "../../Components/CheckoutFom";
-// import { Elements } from "@stripe/react-stripe-js";
+
 
 const Dashboard = () => {
 
-  const { user } = useContext(AuthContext);
-
   return (
     <div className="">
-      <div>
-        <div className="">
+      {/* First line of dashboard */}
+      <div className="grid grid-cols-3 gap-6">
+        <div className="py-20 px-14 bg-white dashShadow rounded-[10px]">
+          <p className="text-center text-[96px] leading-[116px]">$00</p>
+          <p className="text-center text-2xl leading-[29px]">Balance</p>
+        </div>
+        <div className="bg-white dashShadow rounded-[10px]"></div>
+        <div className="bg-white dashShadow rounded-[10px] flex flex-col justify-center items-center">
+          <p className="text-center mb-4 font-semibold text-xl">ACTIVATE YOUR ACCOUNT</p>
+          <button className="text-2xl font-semibold bg-[#FFE500] rounded-[10px] px-14 py-4">ACTIVE</button>
+        </div>
+      </div>
 
+      {/* Second line of dashboard */}
+      <div className="grid grid-cols-3 gap-6 mt-6">
+        <div className="bg-white col-span-2 dashShadow rounded-[10px] py-10">
+          <h4 className="ml-4 text-2xl font-bold">TRANSACTION</h4>
+        </div>
+        <div className="bg-white dashShadow rounded-[10px] py-10">
+          <h4 className="ml-4 text-2xl font-bold">OUR CLIENTS</h4>
         </div>
       </div>
     </div>
--- src/Page/Dashboard/Dashboardtitle.jsx
+++ src/Page/Dashboard/Dashboardtitle.jsx
@@ -6,7 +6,7 @@ const Dashboardtitle = () => {
   const { user } = useContext(AuthContext);
   return (
     <div className="flex items-center justify-between mt-4 mx-4">
-      <div className="bg-white text-black text-3xl font-semibold py-4 pl-4 pr-16 rounded-xl dashShadow">
+      <div className="bg-white text-black text-3xl font-semibold py-4 pl-4 pr-20 rounded-xl dashShadow">
         <p className="mb-2">ID:*************</p>
         <p>Balance: $00</p>
       </div>
