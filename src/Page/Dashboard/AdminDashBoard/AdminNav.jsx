7c8827b9f310b7c971945c2b9774213418b582fc
Sakib Rahman Safat <sakibsafat47@gmail.com>
2023-08-16 21:53:50 +0600

    Add funds updated

--- src/Page/Dashboard/AdminDashBoard/AdminNav.jsx
+++ src/Page/Dashboard/AdminDashBoard/AdminNav.jsx
@@ -14,67 +14,70 @@ const AdminNav = () => {
   return (
     <div className="flex  items-center lg:items-start bg-white">
       <div>
-        <div>
-          {
-            menu === false ? <Icon onClick={() => setMenu(!menu)} icon="material-symbols:menu" className="lg:hidden text-4xl ml-2 pt-2" />
-              :
-              <Icon onClick={() => setMenu(!menu)} icon="akar-icons:cross" className="lg:hidden text-4xl ml-2 pt-2" />
-          }
 
-        </div>
-        <div className={menu === true ? "w-full lg:w-[328px] lg:flex flex-col items-center rounded-none bg-white h-full" : "w-1/2 lg:w-[110px] lg:flex flex-col items-center rounded-none sticky top-0 bg-white h-full hidden"}>
+        {
+          menu === false ? <Icon onClick={() => setMenu(!menu)} icon="material-symbols:menu" className="lg:hidden text-4xl ml-2 pt-2" />
+            :
+            <Icon onClick={() => setMenu(!menu)} icon="akar-icons:cross" className="lg:hidden text-4xl ml-2 pt-2" />
+        }
 
-          <div className="sticky top-0">
+
+        <div className={menu === true ? "w-full lg:w-[110px] font-red lg:flex flex-col  rounded-none bg-white h-full" : "w-1/2 lg:w-[110px] font-red lg:flex flex-col  rounded-none sticky top-0 bg-white h-full hidden"}>
+          <Icon icon="material-symbols:menu" className="hidden lg:block text-4xl ml-2 mt-12" />
+          <div className="sticky top-0 ">
             <img
-              className=" pt-2 mt-4 lg:mt-14 mx-auto"
+              className="pt-4 mt-2 w-[75px] lg:mt-14 ml-3"
               src="https://i.ibb.co/JB9x4Wx/Group-1000003448.png"
               alt="LOGO"
             />
-            <p className="text-white mt-4 text-center font-red">{user?.displayName}</p>
+            <p className="text-black mt-4 mx-3 text-[14px] font-red">{user?.displayName}</p>
+
+
+            <div className="w-12 mt-6 mb-5 ml-4 border-b-2 border-black"></div>
 
             <Link to="/dashboard/adminorderpage" onClick={toTop} >
-              <p onClick={() => setMenu(!menu)} className="mt-10 font-red  text-white text-center   cursor-pointer hover:bg-[#E9F7FF] hover:text-black mr-4 rounded-md px-3 py-2">
+              <p onClick={() => setMenu(!menu)} className="mt-10 text-black text-[10px] cursor-pointer hover:bg-[#3186EC] hover:text-white font-red rounded-md mx-2 px-3 py-2">
                 Order receive
               </p>
             </Link>
             <Link to="/dashboard/adminrefercode" onClick={toTop} >
-              <p onClick={() => setMenu(!menu)} className="mt-1  text-white text-center   cursor-pointer hover:bg-[#E9F7FF] hover:text-black mr-4 rounded-md px-3 py-2 font-red">
+              <p onClick={() => setMenu(!menu)} className="mt-1 text-black text-[10px]   cursor-pointer hover:bg-[#3186EC] hover:text-white font-red rounded-md mx-2 px-3 py-2 font-red">
                 Refer codes
               </p>
             </Link>
             <Link to="/dashboard/adminpayment" onClick={toTop} >
-              <p onClick={() => setMenu(!menu)} className="mt-1  text-white text-center   cursor-pointer hover:bg-[#E9F7FF] hover:text-black mr-4 rounded-md px-3 py-2">
+              <p onClick={() => setMenu(!menu)} className="mt-1 text-black text-[10px]   cursor-pointer hover:bg-[#3186EC] hover:text-white font-red rounded-md mx-2 px-3 py-2">
                 Payment Received
               </p>
             </Link>
 
 
             <Link to="/dashboard/orders" onClick={toTop} >
-              <p onClick={() => setMenu(!menu)} className="mt-1  text-white text-center   cursor-pointer hover:bg-[#E9F7FF] hover:text-black mr-4 rounded-md px-3 py-2">
+              <p onClick={() => setMenu(!menu)} className="mt-1 text-black text-[10px]   cursor-pointer hover:bg-[#3186EC] hover:text-white font-red rounded-md mx-2 px-3 py-2">
                 Notification
               </p>
             </Link>
             <Link to="/dashboard/userlist" onClick={toTop} >
-              <p onClick={() => setMenu(!menu)} className="mt-1  text-white text-center   cursor-pointer hover:bg-[#E9F7FF] hover:text-black mr-4 rounded-md px-3 py-2">
+              <p onClick={() => setMenu(!menu)} className="mt-1 text-black text-[10px]   cursor-pointer hover:bg-[#3186EC] hover:text-white font-red rounded-md mx-2 px-3 py-2">
                 User List
               </p>
             </Link>
             <Link to="/dashboard/addfunds" onClick={toTop} >
-              <p onClick={() => setMenu(!menu)} className="mt-1  text-white text-center   cursor-pointer hover:bg-[#E9F7FF] hover:text-black mr-4 rounded-md px-3 py-2">
+              <p onClick={() => setMenu(!menu)} className="mt-1 text-black text-[10px]   cursor-pointer hover:bg-[#3186EC] hover:text-white font-red rounded-md mx-2 px-3 py-2">
                 Ticket Receive
               </p>
             </Link>
 
 
             <Link to='/' onClick={toTop} >
-              <p onClick={() => setMenu(!menu)} className="my-10  text-white text-center   cursor-pointer hover:bg-[#E9F7FF] hover:text-black mr-4 rounded-md px-3 py-2">
+              <p onClick={() => setMenu(!menu)} className="mt-1  text-black text-[10px]   cursor-pointer hover:bg-[#3186EC] hover:text-white font-red rounded-md mx-2 px-3 pt-2 mb-10">
                 Go to Home
               </p>
             </Link>
           </div>
         </div>
       </div>
-      <div className={menu === true ? "lg:hidden" : "hidden"}>
+      <div className={menu === true ? "hidden" : "lg:hidden"}>
         <Dashboardtitle />
       </div>
     </div>
--- src/Page/Dashboard/DashAddFunds.jsx
+++ src/Page/Dashboard/DashAddFunds.jsx
@@ -2,15 +2,19 @@
 const DashAddFunds = () => {
     return (
         <div>
-            <form className="flex flex-col w-full">
+            <form className="flex flex-col w-full bg-white rounded-[10px]">
                 <div className="w-full">
-                    <p className='text-xl font-semibold text-black mr-4 mb-2'>Amount:</p>
-                    <input className='h-14 w-full dashShadow rounded-[10px] bg-white' type="text" name='subject' />
+                    
+                    <div className="mx-14 mt-12">
+                        <p className='text-xl font-semibold text-black mr-4 mb-2 font-red '>Amount</p>
+                        <input className='h-14 px-4 dashShadow rounded-[10px] w-full bg-[#E5E5EF] focus:border-0 active:border-0' type="number" name='amount' />
+                        <input type="submit" value="PAY" className="text-2xl w-full font-semibold bg-[#3186EC] rounded-[10px] text-center  py-2 text-white my-6 cursor-pointer" />
+                    </div>
                 </div>
-                <input type="submit" value="Submit" className="text-2xl font-semibold bg-[#FFE500] rounded-[10px] text-center py-4 w-[230px]  my-6 cursor-pointer" />
+                
             </form>
 
-            <div className="bg-white col-span-2 dashShadow rounded-[10px] py-10">
+            <div className="bg-white col-span-2 dashShadow rounded-[10px] py-10 mt-6">
                 <h4 className="ml-4 text-2xl font-bold">TRANSACTION</h4>
             </div>
         </div>
