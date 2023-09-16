f128049ba395069fa31a25d01524f78c13ee0420
Sakib Rahman Safat <sakibsafat47@gmail.com>
2023-09-17 01:38:41 +0600

    dashboard home updated

--- src/Page/Dashboard/DashWelcome.jsx
+++ src/Page/Dashboard/DashWelcome.jsx
@@ -15,10 +15,10 @@ const DashWelcome = () => {
                 </div>
                 
             </div>
-            <div className="bg-black mt-[80px] text-white lg:w-[728px] flex flex-col justify-center items-center lg:rounded-ee-[160px] rounded-ee-[80px] rounded-ss-[80px] lg:rounded-ss-[160px] rounded-b-xl rounded-t-xl">
+            {/* <div className="bg-black mt-[80px] text-white lg:w-[728px] flex flex-col justify-center items-center lg:rounded-ee-[160px] rounded-ee-[80px] rounded-ss-[80px] lg:rounded-ss-[160px] rounded-b-xl rounded-t-xl">
                     <p className='lg:text-xl text-sm font-sans text-center lg:px-[200px] lg:py-10 pt-14 px-6 mb-10'>Activate your account and Use your codes to win two of the best free services.</p>
                     <button className='bg-[#1475EA] text-white py-3 px-14 lg:py-5 lg:px-40 rounded-[10px] lg:mb-10 mb-14'>Active</button>
-            </div>
+            </div> */}
         </div>
     );
 };
