9e787a8e9443fdb2d69c619c211a4e10a024fee9
Sakib Rahman Safat <sakibsafat47@gmail.com>
2023-09-17 02:36:47 +0600

    customerBanner2 img size and one image update

--- src/Components/CustomBanner2.jsx
+++ src/Components/CustomBanner2.jsx
@@ -9,7 +9,7 @@ const CustomBanner2 = ({img, header, para}) => {
                     <h1 className='font-red text-[62px] leading-[72px]'>{header}</h1>
                     <p className='leading-[26px] mt-10'>{para}</p>
                 </div>
-                <img src={img} alt="" />
+                <img className='w-1/2' src={img} alt="" />
             </div>
         </>
     );
