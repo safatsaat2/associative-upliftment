113032ea8449ec048aa7b5ce52da4defa34207da
Sakib Rahman Safat <sakibsafat47@gmail.com>
2023-09-21 21:47:26 +0600

    ticket is going to database

--- src/Page/Dashboard/DashComplainBox.jsx
+++ src/Page/Dashboard/DashComplainBox.jsx
@@ -1,6 +1,11 @@
+import axios from "axios";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const DashComplainBox = () => {
+
+    const navigate = useNavigate()
     // Handling Subject
     const [subject, setSubject] = useState("")
     const handleSubject = (e) => {
@@ -21,14 +26,43 @@ const DashComplainBox = () => {
     // Handle image
     const [image, setImage] = useState("")
     const handleImage =(e) =>{
-        let reader = new FileReader();
-        reader.readAsDataURL(e.target.files[0]);
-        reader.onload= () =>{
-            setImage(reader.result)
+        const img = e.target.files[0]
+        const formData = new FormData();
+        formData.append("image", img);
+
+        const url = `https://api.imgbb.com/1/upload?key=${import.meta.env.VITE_IMGBB}`
+
+        fetch(url, {
+            method: "POST",
+            body: formData,
+        }).then(res => res.json()).then(imageData=>{
+            setImage(imageData.data.display_url)
+        })
+    }
+
+    // Handle Submit
+
+    const handleSubmit = (e) =>{
+        e.preventDefault()
+        const info = {subject, message, transaction, image}
+        if(subject !== "" && message !== "" && transaction !== "" && image !== ""){
+            axios.post("http://localhost:7000/tickets", info)
+            .then(res => {
+                console.log(res)
+                if (res.data.insertedId) {
+                  Swal.fire(
+                    'Your funds will be add soon',
+                    'Success'
+                  )
+                  navigate('/dashboard/customorder')
+                }  
+              })
         }
-        reader.onerror = (error) =>{
-            console.log("error", error)
-        }
+        Swal.fire(
+            'Select All',
+            'Failed'
+          )
+        
     }
     
     return (
@@ -40,7 +74,7 @@ const DashComplainBox = () => {
                 <p className=" mb-2 text-black font-red text-center"><span className="text-lg font-semibold font-red">One Ticket, One Solution:</span> To ensure efficient handling of your concern, please avoid creating multiple tickets for the same issue. Our team is here to streamline the process for you.</p>
                 <p className=" mb-2 text-black font-red text-center font-semibold">We're here to make your experience smooth and hassle-free. Your satisfaction is our priority! 🌟</p>
             </div>
-            <form className="flex flex-col justify-center items-center w-[864px] mx-auto">
+            <div className="flex flex-col justify-center items-center w-[864px] mx-auto">
                 <div className="w-full">
                     <div className=" mx-4 mt-6">
                         <p className='lg:text-xl font-red font-semibold text-black ml-8 lg:ml-0 lg:mr-4 mb-2'>Subject</p>
@@ -52,12 +86,12 @@ const DashComplainBox = () => {
                         <p className='lg:text-xl font-red font-semibold text-black ml-8 lg:ml-0 lg:mr-4 mb-2'>Transaction</p>
                         <input onChange={handleTransction} className='h-14 w-5/6 ml-5 lg:w-full lg:ml-0 dashShadow rounded-[10px] bg-[#E5E5EF] pl-2' type="text" name='transaction' placeholder="Write Your Transaction id..." />
                         <p className='lg:text-xl font-red font-semibold text-black ml-8 lg:ml-0 lg:mr-4 mb-2'>Screenshot</p>
-                        <input className='bg-[#E5E5EF]' type="file" onChange={handleImage} name='img' />
-                        <input type="submit" value="Submit Ticket" className="lg:text-2xl lg:font-semibold bg-[#3186EC] text-white rounded-[10px] text-center w-full py-4 mx-auto my-6 font-red" />
+                        <input className='bg-[#E5E5EF]' type="file" accept="image/*" onChange={handleImage} name='img' />
+                        <input onClick={handleSubmit} type="submit" value="Submit Ticket" className="lg:text-2xl lg:font-semibold bg-[#3186EC] text-white rounded-[10px] text-center w-full py-4 mx-auto my-6 font-red" />
                     </div>
                     
                 </div>
-            </form>
+            </div>
         </div>
     );
 };
