0e6690c709a634a293b3a417212c0ef6299124cd
Sakib Rahman Safat <sakibsafat47@gmail.com>
2023-10-22 21:20:49 +0600

    our team banner mobile responsive implememntation

--- src/Components/CustomBanner4.jsx
+++ src/Components/CustomBanner4.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const CustomBanner4 = ({ img, header, para }) => {
     return (
-        <div className='flex flex-col lg:flex-row lg:items-center lg:justify-between gap-x-[150px]'>
+        <div className='flex flex-col lg:flex-row lg:items-center lg:justify-between gap-x-[100px]'>
             <img className='w-[235px] mx-auto lg:w-[497px]' src={img} alt="" />
             <div >
                 <h1 className='font-red text-center lg:text-left text-[#149CE6] lg:text-black text-[20px] lg:text-[62px] leading-[30px] py-[30px] lg:leading-[72px]'>{header}</h1>
--- src/Components/OurTeamBanner.jsx
+++ src/Components/OurTeamBanner.jsx
@@ -3,19 +3,19 @@ const OurTeamBanner = ({quote1, quote2, h3, img}) => {
     return (
         <>
             <div style={{ background: "linear-gradient(90deg, #1973E3 32.81%, #000 82.2%)", WebkitBorderTopLeftRadius: "175px", WebkitBorderBottomRightRadius: "65px" }} className="max-w-[1280px] mx-auto">
-                <div className="flex items-center gap-x-[46px]">
-                    <div className="flex flex-col items-center pl-[63px]">
-                        <h3 className="bg-white text-[#1973E3] py-[21px] px-[27px] rounded-[30px] text-[50px] font-bold leading-[74px]">
+                <div className="flex flex-col lg:flex-row items-center gap-x-[46px]">
+                    <div className="flex flex-col items-center px-10 lg:pl-[63px]">
+                        <h3 className="bg-white text-[#1973E3] py-1 px-2 rounded-md text-xl mt-10 lg:mt-0 lg:py-[21px] lg:px-[27px] lg:rounded-[30px] lg:text-[50px] font-bold lg:leading-[74px]">
                             {h3}
                         </h3>
-                        <p className="qoute1 text-center text-white text-xl leading-[30px] mt-[32px]">
+                        <p className="qoute1 text-center text-white text-[12px] lg:text-xl lg:leading-[30px] mt-4 lg:mt-[32px]">
                             {quote1}
                         </p>
-                        {quote2 !== "" ? <p className="quote2 text-center text-white text-xl leading-[30px] mt-6">
+                        {quote2 !== "" ? <p className="quote2 text-center text-white text-[12px] lg:text-xl lg:leading-[30px] mt-4 lg:mt-6">
                             {quote2}
                         </p> : ""}
                     </div>
-                    <img className="pt-[58px] pr-[22px] pb-[32px]" src={img} alt="" />
+                    <img className="pt-[16px] w-5/6 lg:w-auto mx-auto lg:pt-[58px] lg:pr-[22px] pb-[32px]" src={img} alt="" />
                 </div>
             </div>
         </>
